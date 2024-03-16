d92335d2dcaf5beb92b6fa9cada0d3636ec4d9ec
tuananh1105 <150455795+tuananh1105@users.noreply.github.com>
2024-03-17 01:01:10 +0700

    update

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                shop: resolve(__dirname, './shop.html'),
+                detail: resolve(__dirname, './view.html'),
+                cart: resolve(__dirname, './cart.html'),
+                checkout: resolve(__dirname, './check_out.html'),
+                login: resolve(__dirname, './login.html'),
+            },
+        },
+    },
+})
